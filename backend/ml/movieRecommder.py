affc4873e30a23641865bc2a1e648f04de91fdce
ezaanamin <ezaan.amin@gmail.com>
2024-04-11 11:58:59 +0500

    Cleaning dataset

---
+++ backend/ml/movieRecommder.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Cleaning dataset
+'''
+a=pd.read_csv("/home/ezaan-amin/Documents/Portfolio/FlixVerse/backend/ml/moives.csv")
+
+
+
+
+
+data=pd.DataFrame(a)
+
+d10=data.dropna()
+
+d10.to_csv("new_moives.csv",index=0)
+'''
+
